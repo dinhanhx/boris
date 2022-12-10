49f76317832ada2a3c22c1845c99bdab40440bef
dinhanhx <dinhanhx@gmail.com>
2022-12-10 15:18:54 +0700

    n slave

--- experiments/generate_cmds.py
+++ experiments/generate_cmds.py
@@ -14,15 +14,14 @@ machines = {'master': None,
             'slave': []}
 
 for i in response['Reservations']:
-    instance = i['Instances'][0]
-    public_dns_name = instance['PublicDnsName']
-
-    if instance['Tags'][0]['Value'] == 'master':
-        machines['master'] = {'PublicDnsName': instance['PublicDnsName']}
-    if instance['Tags'][0]['Value'] == 'slave':
-        machines['slave'].append({'PublicDnsName': instance['PublicDnsName']})
-
-    print(f'scp -i secrets/general_keypair.pem experiments/10-kubeadm.conf ubuntu@{public_dns_name}:/home/ubuntu/')
+    instance = i['Instances']
+    for ix in instance:
+        public_dns_name = ix['PublicDnsName']
+        if ix['Tags'][0]['Value'] == 'master':
+            machines['master'] = {'PublicDnsName': public_dns_name}
+        if ix['Tags'][0]['Value'] == 'slave':
+            machines['slave'].append({'PublicDnsName': public_dns_name})
+        print(f'scp -i secrets/general_keypair.pem experiments/10-kubeadm.conf ubuntu@{public_dns_name}:/home/ubuntu/')
 
 print(f"ssh -i secrets/general_keypair.pem ubuntu@{machines['master']['PublicDnsName']} 'sudo bash -s' < experiments/master.sh")
 print(f"scp -i secrets/general_keypair.pem ubuntu@{machines['master']['PublicDnsName']}:/home/ubuntu/join.sh secrets/")
